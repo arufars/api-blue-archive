8cdf3990cd8efe03ae8583df5557baa8d66d0433
arufars <firselee@gmail.com>
2022-11-06 17:28:33 +0700

    update link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,13 @@
-import Link from 'next/link'
-
+import Link from "next/link"
 
 const Footer: React.FC = () => {
   return (
     <footer>
       <div className='bg-gray-800 text-gray-400 text-center py-10'>
-        <p className='underline italic mb-2'><Link href="https://github.com/Marineux">Github</Link> 2022</p>
-        <p className='text-sm'>Made with ❤️ by arufars</p>
+        <p className='underline italic mb-2'>
+          <Link href='https://github.com/arufars'>Github</Link>
+        </p>
+        <p className='text-sm'>Made with ❤️ by arufars {new Date().getFullYear()}</p>
       </div>
     </footer>
   )
